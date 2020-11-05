83e9503e8b11decbdd08ae48e5c30cc5938a1ffd
lenovo <lenovo@DESKTOP-32CB9SE>
2020-11-05 15:20:03 +0530

    edits done pls check once

--- src/test/java/mypack/Test1.java
+++ src/test/java/mypack/Test1.java
@@ -5,7 +5,7 @@ public class Test1
 	public static void main(String[] args)
 	{
 		System.out.println("Cts info");
-		System.out.println("edits are done"):
+		System.out.println("edits are done");
 	}
 
 }
